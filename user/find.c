905f99e9730bf4bf1f0e4847efae5a9652deeff2
Rayyan-A1t <sussysus334@gmail.com>
2026-09-16 01:31:37 +0500

    find: add regex pattern matching

--- user/find.c
+++ user/find.c
@@ -7,6 +7,45 @@
 char *exec_argv[MAXARG];
 int exec_argc = 0;
 
+int matchhere(char *, char *);
+int matchstar(int, char *, char *);
+
+int
+match(char *re, char *text)
+{
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+  if (re[0] == '\0')
+    return 1;
+  if (re[1] == '*')
+    return matchstar(re[0], re + 2, text);
+  if (re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+    return matchhere(re + 1, text + 1);
+  return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+  return 0;
+}
+
 void
 run_exec(char *matched_path)
 {
@@ -77,7 +116,7 @@ find(char *path, char *target)
       continue;
     }
 
-    if (strcmp(de.name, target) == 0) {
+    if (match(target, de.name)) {
       if (exec_argc > 0) {
         run_exec(buf);
       } else {
@@ -97,13 +136,13 @@ int
 main(int argc, char *argv[])
 {
   if (argc < 3) {
-    fprintf(2, "Usage: find directory filename [-exec cmd ...]\n");
+    fprintf(2, "Usage: find directory pattern [-exec cmd ...]\n");
     exit(1);
   }
 
   if (argc > 3) {
     if (strcmp(argv[3], "-exec") != 0) {
-      fprintf(2, "Usage: find directory filename [-exec cmd ...]\n");
+      fprintf(2, "Usage: find directory pattern [-exec cmd ...]\n");
       exit(1);
     }
     for (int i = 4; i < argc; i++) {
